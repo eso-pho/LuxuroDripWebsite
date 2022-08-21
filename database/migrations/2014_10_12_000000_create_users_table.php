888975d22cff6f0246a30d3bc48bb061bda2845a
Nitai Keshan <nitai.keshan@study.beds.ac.uk>
2022-08-21 12:29:07 -0400

    This is the first stage of the user management where they can manage their account information in terms of name, email and password changes. They can also enable 2FA authentication as well as log out of other browser session and delete their account.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('phone_number',11);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
+            $table->string('user_type')->default('user');
             $table->timestamps();
         });
     }
