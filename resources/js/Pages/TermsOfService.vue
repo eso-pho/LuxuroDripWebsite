c2293214878c5f1a1a972ed1fbc555c6cb447b4a
Nitai Keshan <nitai.keshan@study.beds.ac.uk>
2022-09-13 23:27:45 -0400

    Update 1.65?
    Started Modifications of Terms and Services???

--- resources/js/Pages/TermsOfService.vue
+++ resources/js/Pages/TermsOfService.vue
@@ -18,6 +18,7 @@ defineProps({
                 </div>
 
                 <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg prose" v-html="terms" />
+
             </div>
         </div>
     </div>
