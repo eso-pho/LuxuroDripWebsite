9df027addc0aa179e4ea32c19725fc6973e8104f
Nitai Keshan <nitai.keshan@study.beds.ac.uk>
2022-09-25 10:24:34 -0400

    Update 1.69?
    Uninstall DaisyUI,
    Added new packages from NPM

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,11 @@
 import './bootstrap';
 import '../css/app.css';
-
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -19,5 +19,4 @@ createInertiaApp({
             .mount(el);
     },
 });
-
 InertiaProgress.init({ color: '#4B5563' });
